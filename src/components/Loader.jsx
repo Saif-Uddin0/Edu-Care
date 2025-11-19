232a92629f08cb9b7d5488c8a9ab6970c4b486bb
Saif Uddin <mahirr.846@gmail.com>
2025-11-20 02:25:36 +0600

    added the laoder

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,16 @@
 "use client";
 
 import React from "react";
-import ClipLoader from "react-spinners/ClipLoader";
+import { HashLoader } from "react-spinners";
 
-const Loader = ({ size = 60, color = "#0d9488", fullScreen = false }) => {
+const Loader = ({ size = 100, color = "#39B8AD", fullScreen = false }) => {
   return (
     <div
       className={`flex items-center justify-center ${
         fullScreen ? "h-screen w-full" : "py-10"
       }`}
     >
-      <ClipLoader size={size} color={color} speedMultiplier={1.2} />
+      <HashLoader size={size} color={color} speedMultiplier={1.2} />
     </div>
   );
 };
